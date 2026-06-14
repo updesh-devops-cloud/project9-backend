fd0c217112056576b262250ea3cbf5fea2529f9e
updesh-devops <updeshpanwar123@gmail.com>
2026-06-14 15:53:00 +0530

    Added Virtual Network and Subnet

--- main.tf
+++ main.tf
@@ -24,4 +24,18 @@ resource "azurerm_storage_container" "tfstate" {
   storage_account_id    = azurerm_storage_account.backend_sa.id
   container_access_type = "private"
 }
+# Naya Virtual Network (VNet)
+resource "azurerm_virtual_network" "vnet" {
+  name                = "project9-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
+# Us VNet ke andar ek Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
